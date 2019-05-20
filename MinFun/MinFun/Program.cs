b72f9bc5d5d2f589acd6d04b1327ac59c083ad4b
evy-goryunov <evy.goryunov@gmail.com>
2019-05-21 00:27:47 +0300

    4

--- MinFun/MinFun/Program.cs
+++ MinFun/MinFun/Program.cs
@@ -18,76 +18,122 @@ using System.IO;
 namespace MinFun
 {
 
-	public delegate double MyDel (double x, double a);
+	public delegate double MyDel (double x);
 
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			int count = 0;
+			int min = 0;
+			int max = 0;
+			//коллекция делегатов
 			List<MyDel> listOfFun = new List<MyDel>();
 			listOfFun.Add(F1);
 			listOfFun.Add(F2);
+			listOfFun.Add(F3);
 
+			//выбираем функцию и отрезок где искать минимум, парсим.
+			Console.WriteLine("Введите отрезок функции");
+			min = Int32.Parse(Console.ReadLine());
+			max = Int32.Parse(Console.ReadLine());
+			Console.WriteLine("Введите 1 что бы получить минимум функции: x * x - 50 * x + 10 \n" +
+							  "Введите 2 что бы получить минимум функции: x * x \n" +
+							  "Введите 3 что бы получить минимум функции: Sin(x)");
+			count = Int32.Parse(Console.ReadLine());
 
-			SaveFunc("data.bin", -100, 100, 0.5);
-			Console.WriteLine(Load("data.bin"));
-			Console.ReadKey();
+			//логика выбора функции
+			if (count == 1)
+			{
+				SaveFunc(listOfFun[0], "data.bin", min, max, 0.5);
+			}
+			if (count == 2)
+			{
+				SaveFunc(listOfFun[1], "data.bin", min, max, 0.5);
+			}
+			if (count == 3)
+			{
+				SaveFunc(listOfFun[2], "data.bin", min, max, 0.5);
+			}
+
 		}
 
-		// функции
+		//функции
 		public static double F1(double x)
 		{
 				return x * x - 50 * x + 10;
 		}
 
-		public static double F2(double x, double a)
+		public static double F2(double x)
 		{
-			return a * (x * x);
+			return x * x;
 		}
 
-		public static double F3(double x, double a)
+		public static double F3(double x)
 		{
-			return a * Math.Sin(x);
+			return Math.Sin(x);
 		}
 
-		// запись данных работы функции в файл
-		public static void SaveFunc(string fileName, double a, double b, double h)
+		//запись данных работы функции в файл
+		public static void SaveFunc(MyDel F, string fileName, double a, double b, double h)
 		{
 			FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
 			BinaryWriter bw = new BinaryWriter(fs);
 			double x = a;
 			while (x <= b)
 			{
-				bw.Write(F1(x));
-				x += h;  // x=x+h;
+				bw.Write(F(x));
+				x += h;  //x=x+h;
 			}
 			bw.Close();
 			fs.Close();
+
+			Console.WriteLine(Load("data.bin"));
+			Console.ReadKey();
 		}
 
-		// чтение данных работы функции из файла и нахождение минимум функции
+		//чтение данных работы функции из файла и нахождение минимум функции
+		//public static double Load(string fileName)
+		//{
+
+		//	FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+		//	BinaryReader bw = new BinaryReader(fs);
+		//	double min = double.MaxValue;
+		//	double d;
+		//	//нахождение минимума
+		//	for (int i = 0; i < fs.Length / sizeof(double); i++)
+		//	{
+		//		//Считываем значение и переходим к следующему
+		//		d = bw.ReadDouble();
+		//		if (d < min) min = d;
+		//	}
+		//	bw.Close();
+		//	fs.Close();
+		//	return min;
+		//}
+
 		public static double Load(string fileName)
 		{
-
+			List<double> minimumList = new List<double>();
 			FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
 			BinaryReader bw = new BinaryReader(fs);
 			double min = double.MaxValue;
 			double d;
-			// нахождение минимума
+			//нахождение минимума
 			for (int i = 0; i < fs.Length / sizeof(double); i++)
 			{
-				// Считываем значение и переходим к следующему
+				minimumList.Add(bw.ReadDouble());
+				//Считываем значение и переходим к следующему
 				d = bw.ReadDouble();
 				if (d < min) min = d;
+			}
+			foreach (var e in minimumList)
+			{
+				Console.WriteLine(e);
 			}
 			bw.Close();
 			fs.Close();
 			return min;
 		}
-				
-		
-
-				
-			
 	}
 }
